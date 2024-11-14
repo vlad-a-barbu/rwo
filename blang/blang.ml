c17e1150ed7d131f565c31c1488846c19af4bc12
Vlad A. Barbu <vlad.a.brb@gmail.com>
2024-11-14 16:26:46 +0200

    simple boolean lang evaluator + basic cohttp server

---
+++ blang/blang.ml
@@ -0,0 +1,18 @@
+type boolean_op =
+  | Base of bool
+  | And of boolean_op * boolean_op
+  | Or of boolean_op * boolean_op
+  | Xor of boolean_op * boolean_op
+  | Not of boolean_op
+
+let rec eval expr =
+  match expr with
+  | Base x -> x
+  | And (x, y) -> eval x && eval y
+  | Or (x, y) -> eval x || eval y
+  | Xor (x, y) -> eval x <> eval y
+  | Not x -> not (eval x)
+
+let%test "test" =
+  eval (And (Or (Base true, Base false), Not (And (Base true, Base false))))
+  = true
