755602e44c6d261dfc68c1286c666f2d0d1e92b9
Vlad A. Barbu <vlad.a.brb@gmail.com>
2024-11-25 22:32:38 +0200

    learn algebraic effects WIP - simple shell

--- effects/effects.ml
+++ effects/effects.ml
@@ -3,65 +3,123 @@
 open Effect
 open Effect.Deep
 
-type _ Effect.t += Conversion_failure : string -> int Effect.t
+type _ Effect.t += Print : string -> unit Effect.t
 
-let int_of_string l =
-  try int_of_string l with
-  | Failure _ -> perform (Conversion_failure l)
+let print msg = perform @@ Print msg
+
+let print_thrice () =
+  print "hello";
+  print "hello";
+  print "hello"
 ;;
 
-let rec sum_up acc =
-  let l = input_line stdin in
-  acc := !acc + int_of_string l;
-  sum_up acc
-;;
-
-let example1 () =
-  Printf.printf "Starting up. Please input:\n%!";
-  let r = ref 0 in
-  match_with
-    sum_up
-    r
-    { effc =
-        (fun (type c) (eff : c Effect.t) ->
-          match eff with
-          | Conversion_failure s ->
-            Some
-              (fun (k : (c, _) continuation) ->
-                Printf.fprintf stderr "Conversion failure \"%s\"\n%!" s;
-                continue k 0)
-          | _ -> None)
-    ; exnc =
-        (function
-          | End_of_file -> Printf.printf "Sum is %d\n" !r
-          | e -> raise e)
-    ; (* Shouldn't reach here, means sum_up returned a value *)
-      retc = (fun _ -> failwith "Impossible, sum_up shouldn't return")
-    }
-;;
-
-type 'a Effect.t += EffExn : exn -> 'a Effect.t
-
-let raise_eff e = perform (EffExn e)
-
-exception TestExn of string
-
-let eff_exn_example () =
-  (* let ( // ) x y = if y = 0 then raise_eff @@ TestExn "div by 0" else x / y in *)
-  let calc () = 3 / 0 in
-  (* let calc_eff () = 3 // 0 in *)
-  match_with
-    calc
+let _ex1 () =
+  try_with
+    print_thrice
     ()
     { effc =
-        (fun (type c) (eff : c Effect.t) ->
-          match eff with
-          | EffExn _exn ->
-            Some (fun (_k : (c, _) continuation) -> print_endline "Eff exn performed")
+        (fun (type c) (e : c Effect.t) ->
+          match e with
+          | Print msg ->
+            print_endline msg;
+            None
+          | _ -> None)
+    }
+;;
+
+let _ex2 () =
+  try_with
+    print_thrice
+    ()
+    { effc =
+        (fun (type c) (e : c Effect.t) ->
+          match e with
+          | Print msg ->
+            print_endline msg;
+            Some (fun (k : (c, _) continuation) -> continue k ())
+          | _ -> None)
+    }
+;;
+
+(* simple shell *)
+
+type cmd =
+  | CD of string
+  | LS of string
+
+type _ Effect.t += Cmd : cmd -> string Effect.t
+
+exception InvalidCmd of string
+
+let single_arg_cmd_re cmd =
+  let pat = Printf.sprintf "[ \\t]*%s[ \\t]+([a-zA-Z0-9\\/\\.]+)[ \\t]*" cmd in
+  (* print_endline pat; *)
+  Re.Posix.compile_pat pat
+;;
+
+let cmds = [ "cd", single_arg_cmd_re "cd"; "ls", single_arg_cmd_re "ls" ]
+
+let eval_cmd cmd =
+  let eval (c, re) =
+    try
+      let matches = Re.exec re cmd in
+      let arg = Re.Group.get matches 1 in
+      Some (c, arg)
+    with
+    | _ -> None
+  in
+  List.find_map eval cmds
+;;
+
+let read_cmd () =
+  let cmd = input_line stdin in
+  match eval_cmd cmd with
+  | Some ("cd", arg) -> CD arg
+  | Some ("ls", arg) -> LS arg
+  | _ -> raise @@ InvalidCmd cmd
+;;
+
+let loop_rec loop =
+  let cmd = read_cmd () in
+  let res = perform @@ Cmd cmd in
+  print_endline res;
+  loop ()
+;;
+
+let ls_and_print path =
+  Sys.readdir path
+  |> Array.fold_left (fun acc next -> Printf.sprintf "%s\n%s" acc next) ""
+;;
+
+let handle_cmd = function
+  | CD p ->
+    Sys.chdir p;
+    ""
+  | LS p -> ls_and_print p
+;;
+
+let rec loop () =
+  Printf.printf "$ %!";
+  match_with
+    loop_rec
+    loop
+    { effc =
+        (fun (type c) (e : c Effect.t) ->
+          match e with
+          | Cmd cmd ->
+            Some (fun (k : (c, _) continuation) -> continue k @@ handle_cmd cmd)
           | _ -> None)
     ; exnc =
         (function
-          | _ -> print_endline "Std exn raised")
-    ; retc = (fun x -> Printf.printf "res = %d\n" x)
+          | InvalidCmd cmd ->
+            Printf.printf "invalid cmd: %s\n%!" cmd;
+            loop ()
+          | e -> raise e)
+    ; retc = (fun _ -> assert false)
     }
+;;
+
+let () =
+  print_endline "simple shell to learn & demo algebraic effects";
+  loop ()
 ;;
